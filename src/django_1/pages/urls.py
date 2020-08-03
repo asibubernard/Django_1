3f724b0a93cc76994b8d251d8f845f40bec4996e
asibubernard <44650300+asibubernard@users.noreply.github.com>
2020-08-03 17:53:45 +0000

    Update urls.py
    modified code to use Pep8 rule in ensuring high quality and readable codes.

--- src/django_1/pages/urls.py
+++ src/django_1/pages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import homePageView
+from .views import home_page_view
 
 urlpatterns = [
-    path('', homePageView, name='home'),
+    path('', home_page_view, name='home'),
     ]
